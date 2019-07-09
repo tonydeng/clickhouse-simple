9c32e68d4c8dc2d2d399fddeaed70d3fc34623e8
Tony Deng <wolf.deng@gmail.com>
2019-07-09 14:10:08 +0800

    :white_check_mark: test(1.0): 添加BaseTest
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

---
+++ src/test/java/com/github/tonydeng/samples/BaseTest.java
@@ -0,0 +1,13 @@
+package com.github.tonydeng.samples;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class BaseTest {
+
+    @Test
+    void test(){
+        log.info("test");
+    }
+}
